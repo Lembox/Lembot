0a9fb25960297bcd8af3ab6b69fa2a73b854e250
Lembox <thelembox@gmail.com>
2018-07-23 00:10:14 +0200

    Giving a method to announce an outage of the bot to fix issues or update the code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 import core.Lembot;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Lembot bot = new Lembot();
+
+        String input = scanner.next();
+        if (input.equals("q")) {
+            bot.announceOfftime();
+            System.exit(-9);
+        }
     }
+
 }
